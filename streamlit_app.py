769bce910b635642343bc205923b93d7e3556a25
vinhhung053 <54439894+vinhhung053@users.noreply.github.com>
2024-08-04 17:16:11 +0000

    update selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
 )
+
+st.write("Ban muon :", option)
